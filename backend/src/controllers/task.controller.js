c3042917eb2294af7e17263dd8e18370be9abed1
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2026-02-08 19:40:03 +0530

    added getTodayTasks and updateTask logic

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -12,6 +12,49 @@ export const addTask = async (req, res) => {
   }
 };
 
+export const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      req.body,            // fields to update
+      { new: true }        // return updated document
+    );
+
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update task", error });
+  }
+};
+
+// get today's tasks from the database and send it to the frontend
+export const getTodayTasks = async (req, res) => {
+  try {
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59, 999);
+
+    const tasks = await Task.find({
+      createdAt: {
+        $gte: startOfDay,
+        $lte: endOfDay,
+      },
+    });
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch today tasks", error });
+  }
+};
+
+// get all the tasks from the database and send it to the frontend
 export const getTasks = async (req, res) => {
   try {
     const tasks = await Task.find();
--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -1,13 +1,14 @@
 // tasks routes are defined here
 import express from 'express';
-import { addTask, getTasks } from '../controllers/task.controller.js';
+import { addTask, getTasks, getTodayTasks, updateTask } from '../controllers/task.controller.js';
 
 const router = express.Router();
 
 router.post('/tasks', addTask);
+router.put('/tasks/:id', updateTask);
 router.get('/tasks', getTasks);
+router.get('/tasks/today', getTodayTasks);
 // router.get('/tasks/:id', getTaskById);
-// router.put('/tasks/:id', updateTask);
 // router.delete('/tasks/:id', deleteTask);
 
 export default router;

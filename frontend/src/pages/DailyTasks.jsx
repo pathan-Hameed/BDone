35172384f39208422f93183bb15d3c9a0b97f0bf
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2026-02-12 21:21:31 +0530

    complete toggle in db fetch request is built, is working

--- frontend/src/pages/DailyTasks.jsx
+++ frontend/src/pages/DailyTasks.jsx
@@ -1,42 +1,62 @@
 import React, { useEffect, useState } from "react";
-import CreateTasks from '../components/CreateTasks.jsx'
+import CreateTasks from "../components/CreateTasks.jsx";
 import TaskItem from "../components/TaskItem.jsx";
 
 export default function DailyTasks() {
   const [tasks, setTasks] = useState([]);
 
-  useEffect( () => {
-    const fetchTasks = async () => {
+  useEffect(() => {
+    const fetchDailyTasks = async () => {
       try {
-            const res = await fetch('http://localhost:5000/api/daily')
-    const data = await res.json();
-    setTasks(data.tasks);
+        const res = await fetch("http://localhost:5000/api/daily");
+        const data = await res.json();
+        setTasks(data.tasks);
       } catch (error) {
         console.log(error);
       }
-    }
-    
-    fetchTasks();
-  }, [])
+    };
 
+    fetchDailyTasks();
+  }, []);
+
+  // DAILY TASK ADDED
   const handleSubmit = async (task) => {
-
-    const res = await fetch('http://localhost:5000/api/daily', {
-      method: 'POST',
-      headers: { 'content-type': 'application/json'},
-      body: JSON.stringify(task)
-    })
+    const res = await fetch("http://localhost:5000/api/daily", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(task),
+    });
 
     const data = await res.json();
-    setTasks([...tasks, data.data])
-  }
+    setTasks([...tasks, data.data]);
+  };
 
-  const toggleTask = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, completed: !task.completed } : task,
+ // TOGGLE COMPLETION STATUS
+  const handleComplete = async (id) => {
+    const previousTasks = [...tasks];
+
+    // optimistic update
+    setTasks((prev) =>
+      prev.map((task) =>
+        task._id === id ? { ...task, isCompleted: !task.isCompleted } : task,
       ),
     );
+
+    try {
+      const res = await fetch(`http://localhost:5000/api/daily/${id}/toggle`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          isCompleted: !tasks.find((t) => t._id === id).isCompleted,
+        }),
+      });
+      const data = await res.json();
+      console.log(data.message);
+      
+    } catch (error) {
+      console.error("faild to toggle",error);
+      setTasks(previousTasks); // rollback
+    }
   };
 
   const deleteTask = (id) => {
@@ -49,31 +69,34 @@ export default function DailyTasks() {
         <div className="mb-6 md:mb-12">
           {/* Header */}
           <h1 className="text-3xl text-center font-bold text-gray-800 mb-2">
-            Today's Tasks
+            Daily Tasks
           </h1>
-          <p className="text-center">All your daily tasks would appear in dashboard.</p>
+          <p className="text-center">
+            All your daily tasks would appear in dashboard.
+          </p>
         </div>
 
-        {/* Add Task */}
-        <div className="flex justify-center gap-3 mb-6 w-full">
-          <CreateTasks onSubmit={handleSubmit}/>
-        </div>
-
-        {/* Task List */}
-        <div className="space-y-4 max-w-2/3 mx-auto">
-          {tasks.map((task) => (
-            <TaskItem 
-            key={task._id}
-            task={task} />
-          ))}
-        </div>
-
-        {/* Empty State */}
-        {tasks.length === 0 && (
-          <div className="text-center text-gray-500 mt-10">
-            No tasks for today 🎉
+        <div className="flex flex-col md:flex-row md:items-start md:justify-center gap-8 w-full ">
+          {/* Add Task */}
+          <div className="flex justify-center gap-3 mb-6 w-full">
+            <CreateTasks onSubmit={handleSubmit} />
           </div>
-        )}
+
+          {/* Task List */}
+          <div className="w-full flex flex-col gap-2 md:gap-4">
+            {tasks.map((task) => (
+              <TaskItem key={task._id} task={task}
+                    onToggleComplete={handleComplete}/>
+            ))}
+          </div>
+
+          {/* Empty State */}
+          {tasks.length === 0 && (
+            <div className="text-center text-gray-500 mt-10">
+              No tasks for today 🎉
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
